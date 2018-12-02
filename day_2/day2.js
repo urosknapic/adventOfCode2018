d43e03d7eab0f277f2dc9fb25d81ec1a5811a03d
uros <urosknapic.develop@gmail.com>
2018-12-02 18:25:59 +0100

    set up for part two

--- day_2/day2.js
+++ day_2/day2.js
@@ -19,36 +19,40 @@ function GetChecksum()
         // display final result
         Helper.ShowOutput("output", "Checksum: " + (threeTimesChecksum * twoTimesChecksum));
     }
+
+    function getCheckSumForLine(line)
+    {
+        var three = 0;
+        var two = 0;
+        var map = new Map();
+
+        for(var i = 0; i<line.length; i++)
+        {
+            var character = line[i];
+            if(map.has(character))
+            {
+                var val = parseInt(map.get(character)) + 1;
+                map.set(character, val);
+            } 
+            else
+            {
+                map.set(character, 1);
+            }
+        }
+        map.forEach(function(val, key){
+            if(val === 2 && two < 1){
+                two++;
+            } 
+            else if( val === 3 && three < 1)
+            {
+                three++;
+            }
+        });
+        
+        return {threeTimes:three, twoTimes:two};
+    }
 };
 
-function getCheckSumForLine(line)
-{
-    var three = 0;
-    var two = 0;
-    var map = new Map();
-
-    for(var i = 0; i<line.length; i++)
-    {
-        var character = line[i];
-        if(map.has(character))
-        {
-            var val = parseInt(map.get(character)) + 1;
-            map.set(character, val);
-        } 
-        else
-        {
-            map.set(character, 1);
-        }
-    }
-    map.forEach(function(val, key){
-        if(val === 2 && two < 1){
-            two++;
-        } 
-        else if( val === 3 && three < 1)
-        {
-            three++;
-        }
-    });
+function GetCommonBoxes(){
     
-    return {threeTimes:three, twoTimes:two};
-}
+};
