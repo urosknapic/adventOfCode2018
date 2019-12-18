54b21968f3ba3fea1da2c665f8c7048e9f3afd3a
uros <urosknapic.develop@gmail.com>
2019-12-18 17:36:36 +0100

    remove some code

--- 2019/day_2/day2.js
+++ 2019/day_2/day2.js
@@ -13,64 +13,7 @@ function readData(event) {
         reader.readAsText(input.files[0]);
 };
 
-function testArrayInt(input){
-    var run = true;
-
-    var index = 0;
-    while(run){
-        var positionOne = index+1;
-        var positionTwo = index+2;
-        var output = index+3;
-
-        var a = input[input[positionOne]];
-        var b = input[input[positionTwo]];
-        switch(input[index]){
-            case 1:
-            input[input[output]] = a + b;
-            break;
-            case 2: 
-            input[input[output]] = a * b;
-            break;
-            case 99: run = false; console.log(input); return; break;
-        }
-        index += 4;
-    }
-}
-
-function WebSoltion(){
-    var ret = 0;
-    // const fs = require('fs');
-    // var data = fs.readFileSync('./input.txt').toString().split('\n');
-    // if(data[data.length-1]==''){data.pop();}
-
-    var ip1pos, ip2pos, oppos;
-    var ip1, ip2, op;
-    // var p=[1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,9,23,1,5,23,27,1,27,9,31,1,6,31,35,2,35,9,39,1,39,6,43,2,9,43,47,1,47,6,51,2,51,9,55,1,5,55,59,2,59,6,63,1,9,63,67,1,67,10,71,1,71,13,75,2,13,75,79,1,6,79,83,2,9,83,87,1,87,6,91,2,10,91,95,2,13,95,99,1,9,99,103,1,5,103,107,2,9,107,111,1,111,5,115,1,115,5,119,1,10,119,123,1,13,123,127,1,2,127,131,1,131,13,0,99,2,14,0,0];
-    // var p = [1,9,10,3,2,3,11,0,99,30,40,50];
-    var p = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,9,23,1,5,23,27,1,27,9,31,1,6,31,35,2,35,9,39,1,39,6,43,2,9,43,47,1,47,6,51,2,51,9,55,1,5,55,59,2,59,6,63,1,9,63,67,1,67,10,71,1,71,13,75,2,13,75,79,1,6,79,83,2,9,83,87,1,87,6,91,2,10,91,95,2,13,95,99,1,9,99,103,1,5,103,107,2,9,107,111,1,111,5,115,1,115,5,119,1,10,119,123,1,13,123,127,1,2,127,131,1,131,13,0,99,2,14,0,0
-    ];
-    // p = data[0].split(',').map(Number);
-    // p[1]=12;
-    // p[2]=2;
-            
-    for(i=0;i<p.length;i+=4){
-        ip1pos = p[i+1];
-        ip2pos = p[i+2];
-        oppos = p[i+3];
-
-        ip1 = p[ip1pos];
-        ip2 = p[ip2pos];
-        
-        if(p[i]==1){op=ip1+ip2;} else if(p[i]==2){op=ip1*ip2;} else if(p[i]==99){break;} else {console.log("ERROR");}
-        p[oppos]=op;
-    }
-
-    ret = p[0];
-
-    console.log(ret);
-}
-
-function testComputer(_inputArray){
+function testComputer(_inputArray, noun, verb){
     // part one
     var oldSubarray = [];
     var finalArray = [];
